657b0dc787eb1bf202d26c828d929c37ed352a77
pcarioufr <pcoid@pm.me>
2024-06-01 00:02:08 +0200

    clean environment variables

--- box.sh
+++ box.sh
@@ -4,13 +4,11 @@ if [ -z "${@}" ] # empty box command
 
 then # opens terminal
     docker compose \
-        --env-file .env \
         run -it --rm -e "TERM=xterm-256color" \
         ubuntu /bin/bash
 
 else # runs a script
     docker compose \
-        --env-file .env \
         run -it --rm \
         ubuntu /opt/box/bin/box ${@}
 
